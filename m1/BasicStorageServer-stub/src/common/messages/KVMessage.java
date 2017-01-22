085dd9a06ac0f2b7c5dbc096b310bff8714f8644
yaronm <yaronm93@gmail.com>
2017-01-22 14:39:46 -0500

    removed functions

--- m1/BasicStorageServer-stub/src/common/messages/KVMessage.java
+++ m1/BasicStorageServer-stub/src/common/messages/KVMessage.java
@@ -21,25 +21,19 @@ public interface KVMessage {
 	 * @return the key that is associated with this message, 
 	 * 		null if not key is associated.
 	 */
-	public String getKey(){
-		return this.key;	
-	}
+	public String getKey();
 	
 	/**
 	 * @return the value that is associated with this message, 
 	 * 		null if not value is associated.
 	 */
-	public String getValue(){
-		return this.value;
-	}
+	public String getValue();
 	
 	/**
 	 * @return a status string that is used to identify request types, 
 	 * response types and error types associated to the message.
 	 */
-	public StatusType getStatus(){
-		return this.stat;
-	}
+	public StatusType getStatus();
 	
 }
 
