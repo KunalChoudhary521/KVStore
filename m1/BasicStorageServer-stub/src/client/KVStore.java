fc3f1dbd8a0f2b8b1956dc59327cb5115ebbbfc9
rahmanz5 <zabeeh.ur.rahman@mail.utoronto.ca>
2017-01-24 10:20:52 -0500

    Got a complete end to end for getTest
    Signed-off-by: rahmanz5 <zabeeh.ur.rahman@mail.utoronto.ca>

--- m1/BasicStorageServer-stub/src/client/KVStore.java
+++ m1/BasicStorageServer-stub/src/client/KVStore.java
@@ -3,6 +3,7 @@ package client;
 
 import app_kvClient.ClientSocketListener;
 import common.messages.KVMessage;
+import javafx.animation.Animation;
 import org.apache.log4j.Logger;
 
 import java.io.InputStream;
@@ -36,14 +37,10 @@ public class KVStore implements KVCommInterface {
 
 	@Override
 	public void connect() throws Exception {
-		this.clientSocket = new Socket(address, port);
 
-		try {
-			this.output = clientSocket.getOutputStream();
-			this.input = clientSocket.getInputStream();
-		} catch(Exception ex){
-			this.logger.trace(ex.getMessage());
-		}
+		this.clientSocket = new Socket(address, port);
+		this.input = clientSocket.getInputStream();
+		this.output = clientSocket.getOutputStream();
 
 		if(clientSocket.isConnected()){
 			String message = "Connection established successffully at " + new Date().toString();
@@ -73,9 +70,9 @@ public class KVStore implements KVCommInterface {
 			{
 				byte[] message = new byte[27];
 
-				byte[] byteKey = new byte[20];
-				for(int i = 0; i < 20; i++){
-					byteKey[i] = (byte) 'K';
+				byte[] byteKey = new byte[key.length()];
+				for(int i = 0; i < key.length(); i++){
+					byteKey[i] = (byte) key.charAt(i);
 				}
 
 				// fill the message with the proper command byte
@@ -85,7 +82,7 @@ public class KVStore implements KVCommInterface {
 				message[1] = (byte) 0;
 
 				// fill the get message with a 20 byte key
-				for(int i = 0; i < 20; i++)
+				for(int i = 0; i < byteKey.length; i++)
 				{
 					message[2+i] = byteKey[i];
 				}
@@ -99,12 +96,61 @@ public class KVStore implements KVCommInterface {
 				for(int i = 0; i < charSize.length; i++){
 					message[22+i] = (byte) charSize[i];
 				}
-				this.clientSocket.getOutputStream().write(message, 0, message.length);
-				//this.clientSocket.getOutputStream().flush();
+				this.output.write(message, 0, message.length);
+				this.output.flush();
+
+				// client blocks until a response from the server is received
+				String serverReply = getResponse();
+
+				Message responseMessage;
+
+				//TODO construct KVMessage correctly
+				if(serverReply != null){
+					responseMessage = new Message(serverReply, serverReply, KVMessage.StatusType.GET_SUCCESS);
+				} else {
+					responseMessage = new Message(null, null, KVMessage.StatusType.GET_ERROR);
+				}
+
+				return responseMessage;
 			}
 		}
 		return null;
 	}
 
-	
+	private String getResponse(){
+		try {
+			byte firstByte = 0;
+			int responseSize = this.input.available();
+			byte[] msgBytes = null;
+			int index = 0;
+
+			// if there is nothing to read block the thread until something becomes available
+			if (responseSize <= 0) {
+				firstByte = (byte) input.read(); // blocking call
+				responseSize = input.available();
+				msgBytes = new byte[responseSize + 1];
+				msgBytes[index] = firstByte;
+				index++;
+			} else {
+				msgBytes = new byte[responseSize];
+			}
+
+			while (responseSize > 0) {
+				msgBytes[index] = (byte) input.read();
+				index++;
+				responseSize = input.available();
+			}
+
+			char[] charMsg = new char[msgBytes.length];
+			for(int i = 0; i < msgBytes.length; i++){
+				charMsg[i] = (char) msgBytes[i];
+			}
+
+			return new String(charMsg);
+
+		}
+		catch(Exception ex){
+			return null;
+		}
+	}
 }
