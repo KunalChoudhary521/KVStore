6a31b84866c7887059054f54631f96120a19ebd6
yaronm <yaronm93@gmail.com>
2017-01-25 22:41:19 -0500

    added tests

--- m1/BasicStorageServer-stub/src/testing/AdditionalTest.java
+++ m1/BasicStorageServer-stub/src/testing/AdditionalTest.java
@@ -3,13 +3,176 @@ package testing;
 import org.junit.Test;
 
 import junit.framework.TestCase;
+import common.messages.KVMessage;
+import common.messages.KVMessage.StatusType;
+import client.KVStore;
 
 public class AdditionalTest extends TestCase {
 	
 	// TODO add your test cases, at least 3
+	private KVStore kvClient;
+	
+	public void setUp() {
+		kvClient = new KVStore("localhost", 50000);
+		try {
+			kvClient.connect();
+		} catch (Exception e) {
+		}
+	}
+
+	public void tearDown() {
+		kvClient.disconnect();
+	}
 	
 	@Test
-	public void testStub() {
+	public void get_long_key() {
+		String key = "The Quick Brown Fox Jumped Over The Lazy Dog";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try{
+			kvClient.get(key);
+		} catch(Exception e){
+			ex = e;
+		}
+		assertTrue(ex == null && response.getStatus() ==StatusType.GET_ERROR);
+	}
+	@Test
+	public void put_long_payload() {
+		String key = "foo";
+		String value = "bar";
+		for (int i = 0; i<=120*1024;i++){
+			value += "bar";
+		}
+		KVMessage response = null;
+		Exception ex = null;
+		try{
+			kvClient.put(key,value);
+			response = kvClient.get(key);
+		}catch(Exception e){
+			ex = e;
+		}
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_ERROR);
+	}
+	@Test
+	public void put_long_key() {
+		String key = "The Quick Brown Fox Jumped Over The Lazy Dog";
+		String value = "bar";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try{
+			kvClient.put(key,value);
+		} catch(Exception e){
+			ex = e;
+		}
+		assertTrue(ex == null && response.getStatus() ==StatusType.PUT_ERROR);
+	}
+	@Test
+	public void delete_get() {
+		String key = "deleteTestValue";
+		String value = "toDelete";
+		
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			kvClient.put(key, value);
+			response = kvClient.put(key, "null");
+			response = kvClient.get(key);
+			
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.GET_ERROR);
+	}
+	@Test
+	public void update_shorter() {
+		String key = "foo";
+		String value = "bar";
+		
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			kvClient.put(key, value);
+			response = kvClient.put(key, "r");
+			
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_UPDATE);
+	}
+	@Test
+	public void update_longer() {
+		String key = "foo";
+		String value = "to";
+		
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			kvClient.put(key, value);
+			response = kvClient.put(key, "The Quick Brown Fox Jumped Over The Lazy Dog");
+			
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_UPDATE);
+	}
+	@Test
+	public void test_get_disconnected() {
+		kvClient.disconnect();
+		String key = "foo";
+		String value = "bar";
+		Exception ex = null;
+
+		try {
+			kvClient.get(key);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertNotNull(ex);
+	}
+	@Test
+	public void test_get_with_0() {
+		String key = "0foo0";
+		String value = "bar";
+		KVMessage response = null;
+		Exception ex = null;
+
+			try {
+				kvClient.put(key, value);
+				response = kvClient.get(key);
+			} catch (Exception e) {
+				ex = e;
+			}
+		
+		assertTrue(ex == null && response.getValue().equals("bar"));
+	}
+	@Test
+	public void test_put_with_0() {
+		String key = "0fo0o0";
+		String value = "0b0a0r";
+		KVMessage response = null;
+		Exception ex = null;
+
+			try {
+				kvClient.put(key, value);
+				response = kvClient.get(key);
+			} catch (Exception e) {
+				ex = e;
+			}
+		
+		assertTrue(ex == null && response.getValue().equals("0b0a0r"));
+	}
+	@Test//need to figure out how to implement
+	public void test_multiple_client_gets() {
 		assertTrue(true);
 	}
+
 }
