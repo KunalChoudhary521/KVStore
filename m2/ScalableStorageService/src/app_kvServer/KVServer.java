7d83302eeee3feb5950b652fdfc37f880721ee8b
rahmanz5 <zabeeh.ur.rahman@mail.utoronto.ca>
2017-02-22 13:43:15 -0500

    initial commit for server

--- m2/ScalableStorageService/src/app_kvServer/KVServer.java
+++ m2/ScalableStorageService/src/app_kvServer/KVServer.java
@@ -1,6 +1,19 @@
 package app_kvServer;
 
 
+import cache.FIFOCache;
+import cache.KVCache;
+import cache.LFUCache;
+import cache.LRUCache;
+import logger.LogSetup;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class KVServer  {
   /**
 	 * Start KV Server at given port
@@ -12,7 +25,170 @@ public class KVServer  {
 	 *           currently not contained in the cache. Options are "FIFO", "LRU", 
 	 *           and "LFU".
 	 */
-	public KVServer(int port, int cacheSize, String strategy) {
-		
+
+    private int port;
+	private int cacheSize;
+	private String strategy;
+	private boolean isRunning;
+	private ServerSocket socket;
+	private static Logger logger = Logger.getRootLogger();
+	private boolean log;
+
+	private String KVFileName;
+
+	private KVCache cache;
+
+	public KVServer(int port, int cSize, String strat, String KVFName, boolean log) {
+		this.port = port;
+		this.cacheSize = cSize;
+		this.strategy = strat;
+		if(cSize <= 0) {//user wants no caching
+			this.cache = null;
+		}
+		else {
+			if (this.strategy.equals("FIFO")) {
+				this.cache = new FIFOCache(this.cacheSize);
+			}
+			else if (this.strategy.equals("LFU")) {
+				this.cache = new LFUCache(this.cacheSize);
+			}
+			else if (this.strategy.equals("LRU")) {
+				this.cache = new LRUCache(this.cacheSize);
+			}
+			else {
+				this.cache = null;
+			}
+		}
+		this.KVFileName = KVFName;
+		this.log = log;
 	}
+
+	public KVCache getKvcache(){return this.cache;}
+
+	/**
+	 * Repeatedly listen for incoming connections, once a connection is recieved,
+	 * pass it to a worker thread. The work done by a worker thread is defined in
+	 * ClientConnection
+	 * */
+	public void Run(){
+
+		isRunning = InitializeServer();
+		if(socket != null) {
+			while(isRunning){
+				try {
+					Socket client = socket.accept();
+					ClientConnection connection =
+							new ClientConnection(client,this, this.KVFileName, this.log);
+					new Thread(connection).start();
+
+					if(log) {
+						logger.info("Connected to "
+								+ client.getInetAddress().getHostName()
+								+ " on port " + client.getPort());
+					}
+				} catch (IOException e) {
+					if(log) {
+						logger.error("Error! " +
+								"Unable to establish connection. \n", e);
+					}
+				}
+			}
+		}
+		if(log) {
+			logger.info("Server stopped.");
+		}
+	}
+
+	/**
+	 * Stops the server
+	 * */
+	public void Stop(){
+		isRunning = false;
+		try {
+			socket.close();
+		} catch (IOException e) {
+			logger.error("Error! " +
+					"Unable to close socket on port: " + port, e);
+
+		}
+	}
+
+	/**
+	 * Initializes the server by trying to obtain a socket on the port
+	 * */
+	private boolean InitializeServer(){
+		logger.info("Initialize server ...");
+
+		try {
+			socket = new ServerSocket(port);
+			logger.info("Server listening on port: "
+					+ socket.getLocalPort());
+
+			return true;
+
+		} catch (IOException e) {
+			logger.error("Error! Cannot open server socket:");
+			if(e instanceof BindException){
+				logger.error("Port " + port + " is already bound!");
+			}
+			return false;
+		}
+	}
+
+	/**
+	 * KVServer contains a global cache and provides an interface to threads
+	 * for adding to a cache
+	 * @param k the key
+	 * @param v the value
+	 * */
+	public void addToCache(String k, String v)
+	{
+
+		if (this.cacheSize >0) {
+			logger.debug("adding "+k+" : "+ v+"to cache");
+			this.cache.insertInCache(k, v);
+		}
+	}
+
+	/**
+	 * KVServer contains a global cache and provides an interface to threads
+	 * for finding something in the cache
+	 * @param k the key
+	 * @param log variable to toggle logging
+	 * */
+	public String findInCache(String k, boolean log)
+	{
+		if (this.cacheSize == 0)
+			return null;
+
+		String val = this.cache.checkCache(k,log);
+		logger.debug("found " + val +"in cache");
+		return val;
+	}
+
+	/**
+	 * Entry point for KVServer
+	 * Parses command line arguments and starts a KVServer
+	 * @param args an array of string command line arguments
+	 * */
+	public static void main(String[] args){
+		// todo validation
+		int port = Integer.parseInt(args[0]);
+		int cacheSize = Integer.parseInt(args[1]);
+		String strategy = args[2];
+		String KVFileName = args[3];
+		boolean shouldLog = Boolean.parseBoolean(args[4]);
+
+		try {
+			new LogSetup(System.getProperty("user.dir")+"/logs/server.log", Level.ALL);
+		} catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+
+		KVServer server = new KVServer(port, cacheSize, strategy, KVFileName, shouldLog);
+		server.Run();
+	}
+
+
 }
