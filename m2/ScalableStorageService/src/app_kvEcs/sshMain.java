b4b5e1a5c81d3adb8af689f55020c02cfd0a4af0
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2017-02-22 14:28:33 -0500

    Updated directory and java commands for m2.

--- m2/ScalableStorageService/src/app_kvEcs/sshMain.java
+++ m2/ScalableStorageService/src/app_kvEcs/sshMain.java
@@ -5,17 +5,19 @@ public class sshMain
     public static void main(String[] args)
     {
         sshSession mySsh = new sshSession();
-        String user = "", host = "ug222.eecg.toronto.edu";//128.100.13.<>
+        String user = "choudh65", host = "ug222.eecg.toronto.edu";//128.100.13.<>
         int port = 22;
 
         mySsh.connectSsh(user,host,port);
 
         String[] serverCmds = {
-                "java -jar ms1-server.jar 8000 100 LRU ../TestFile.txt true",
-                "java -jar ms1-server.jar 8001 100 FIFO ../TestFile.txt true",
-                "java -jar ms1-server.jar 8002 100 LFU ../TestFile.txt true"
+                "java -jar ms1-server.jar 9000 100 LRU ../TestFile.txt true",
+                "java -jar ms1-server.jar 9001 100 FIFO ../TestFile.txt true",
+                "java -jar ms1-server.jar 9002 100 LFU ../TestFile.txt true"
         };
-        String directory = "cd ece419/m1/BasicStorageServer-stub";
+        String directory;
+        directory = "cd ece419/m1/BasicStorageServer-stub";
+        //directory = "cd ece419/m2/ScalableStorageService";
 
         mySsh.runServer(directory, serverCmds);
 
