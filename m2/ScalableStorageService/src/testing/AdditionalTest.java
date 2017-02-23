10dccb0d0cf90cbf3ed15ace06b31015f397c90c
yaronm <yaronm93@gmail.com>
2017-02-23 11:04:49 -0500

    untested commit of KVClient so that I can pull changes to Server. KVClient should have all M2 functionality now.

--- m2/ScalableStorageService/src/testing/AdditionalTest.java
+++ m2/ScalableStorageService/src/testing/AdditionalTest.java
@@ -7,9 +7,56 @@ import junit.framework.TestCase;
 public class AdditionalTest extends TestCase {
 	
 	// TODO add your test cases, at least 3
-	
+
+	//connect to 1 server perform put on different server
 	@Test
-	public void testStub() {
+	public void testStub1() {
+		assertTrue(true);
+	}
+
+	//connect to 1 server perform get on different server
+	@Test
+	public void testStub2() {
+		assertTrue(true);
+	}
+	//perform put on locked server
+	@Test
+	public void testStub3() {
+		assertTrue(true);
+	}
+	//perform put on stopped server
+	@Test
+	public void testStub4() {
+		assertTrue(true);
+	}
+	//perform get on stopped server
+	@Test
+	public void testStub5() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testStub6() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testStub7() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testStub8() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testStub9() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testStub10() {
 		assertTrue(true);
 	}
 }
