3923bb9e6fd4c97067029226efefa4cf76def277
rahmanz5 <zabeeh.ur.rahman@mail.utoronto.ca>
2017-02-23 08:57:25 -0500

    changing the md5 code to accommodate filestorehelper

--- m2/ScalableStorageService/src/app_kvEcs/md5.java
+++ m2/ScalableStorageService/src/app_kvEcs/md5.java
@@ -5,34 +5,18 @@ import java.math.*;
 
 public class md5 
 {
-    /*  Don't create an instance of md5 class, just call
-        this function as: md5.getHash("abc")
-    */
-    public static String getHash(String s)
-    {
-       try
-       {
-           //s can be a KV-pair or KVServer's <IP>:<Port>
-           MessageDigest m = MessageDigest.getInstance("MD5");
-           m.update(s.getBytes(), 0, s.length());
-           //System.out.println("MD5: "+ new BigInteger(1,m.digest()).toString(16));
-           BigInteger a = new BigInteger(1, m.digest());
-           return (new BigInteger(1, m.digest()).toString(16));
-       }
-       catch(Exception ex)
-       {
-           System.out.println("--------md5 of " + s + " failed--------");
-           return null;
-       }
+   public static BigInteger HashBI(String s) throws Exception
+   {
+      MessageDigest m= MessageDigest.getInstance("MD5");
+      m.update(s.getBytes(),0,s.length());
+      return new BigInteger(1,m.digest());
    }
 
-    public static void main(String[] args)
-    {
-        String input = "192.168.0.1:8000";
-        String hexStr = md5.getHash(input);
-        System.out.println("md5 Hash in hex-string " +  hexStr);
+   public static String HashS(String s) throws Exception
+   {
+      MessageDigest m= MessageDigest.getInstance("MD5");
+      m.update(s.getBytes(),0,s.length());
+      return new BigInteger(1,m.digest()).toString(16);
+   }
 
-        BigInteger hash = new BigInteger(hexStr,16);
-        System.out.println("md5 Hash in BigInteger " +  hash);
-    }
 }
