173376d89221b9ede04ea00d1ab9afbe36dacf3c
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2017-04-30 16:22:12 -0400

    Added connect and disconnect functionality for KVServer & KVClient.

--- Milestone1/BasicStorageServer-stub/src/app_kvServer/KVServer.java
+++ Milestone1/BasicStorageServer-stub/src/app_kvServer/KVServer.java
@@ -1,7 +1,22 @@
 package app_kvServer;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import logger.LogSetup;
 
-public class KVServer  {
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class KVServer extends Thread
+{
+
+    private static Logger logger = Logger.getRootLogger();
+
+    private int port;
+    private ServerSocket serverSocket;
+    private boolean running;
 	
 	/**
 	 * Start KV Server at given port
@@ -13,7 +28,124 @@ public class KVServer  {
 	 *           currently not contained in the cache. Options are "FIFO", "LRU", 
 	 *           and "LFU".
 	 */
-	public KVServer(int port, int cacheSize, String strategy) {
-		
-	}
+	public KVServer(int port, String logLevel, String kvFile, int cacheSize, String strategy)
+	{
+		this.port = port;
+        setLogLevel(logLevel);
+		//cacheSize
+        //strategy
+}
+
+    private String setLogLevel(String levelString) {
+
+        if(levelString.equals(Level.ALL.toString())) {
+            logger.setLevel(Level.ALL);
+            return Level.ALL.toString();
+        } else if(levelString.equals(Level.DEBUG.toString())) {
+            logger.setLevel(Level.DEBUG);
+            return Level.DEBUG.toString();
+        } else if(levelString.equals(Level.INFO.toString())) {
+            logger.setLevel(Level.INFO);
+            return Level.INFO.toString();
+        } else if(levelString.equals(Level.WARN.toString())) {
+            logger.setLevel(Level.WARN);
+            return Level.WARN.toString();
+        } else if(levelString.equals(Level.ERROR.toString())) {
+            logger.setLevel(Level.ERROR);
+            return Level.ERROR.toString();
+        } else if(levelString.equals(Level.FATAL.toString())) {
+            logger.setLevel(Level.FATAL);
+            return Level.FATAL.toString();
+        } else if(levelString.equals(Level.OFF.toString())) {
+            logger.setLevel(Level.OFF);
+            return Level.OFF.toString();
+        } else {
+            return LogSetup.UNKNOWN_LEVEL;
+        }
+    }
+
+    private boolean isRunning()
+    {
+        return this.running;
+    }
+
+    public void run()
+    {
+        this.running = initializeServer();
+        if(serverSocket != null)
+        {
+            while(isRunning())
+            {
+                try
+                {
+                    Socket client = serverSocket.accept();
+                    ClientConnection connection = new ClientConnection(client);
+                    new Thread(connection).start();
+
+                    logger.info("Connected to "
+                            + client.getInetAddress().getHostName()
+                            +  " on port " + client.getPort());
+                } catch (IOException e) {
+                    logger.error("Error! " +
+                            "Unable to establish connection. \n", e);
+                }
+            }
+        }
+        logger.info("Server stopped.");
+    }
+
+    private boolean initializeServer()
+    {
+        logger.info("Initialize server ...");
+        try {
+            serverSocket = new ServerSocket(port);
+            logger.info("Server listening on port: "
+                    + serverSocket.getLocalPort());
+            return true;
+
+        } catch (IOException e) {
+            logger.error("Error! Cannot open server socket:");
+            if(e instanceof BindException){
+                logger.error("Port " + port + " is already bound!");
+            }
+            return false;
+        }
+    }
+
+    public void stopServer()
+    {
+        running = false;
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            logger.error("Error! " +
+                    "Unable to close socket on port: " + port, e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new LogSetup("logs/server.log", Level.ALL);//initially, logLevel is ALL
+            if(args.length != 5) {
+                System.out.println("Error! Invalid number of arguments!");
+                System.out.println("Usage: Server <port>!");
+            } else {
+                int port = Integer.parseInt(args[0]);
+                String logLevel = args[1];
+                String kvFile = args[2];
+                int cacheSize = Integer.parseInt(args[3]);
+                String strategy = args[4];
+
+                new KVServer(port, logLevel, kvFile, cacheSize, strategy).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Error! Unable to initialize logger!");
+            e.printStackTrace();
+            System.exit(1);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Error! Invalid argument <port>! Not a number!");
+            System.out.println("Usage: Server <port>!");
+            System.exit(1);
+        }
+    }
 }
--- Milestone1/BasicStorageServer-stub/src/client/KVStore.java
+++ Milestone1/BasicStorageServer-stub/src/client/KVStore.java
@@ -2,39 +2,95 @@ package client;
 
 
 import common.messages.KVMessage;
+import org.apache.log4j.Logger;
 
-public class KVStore implements KVCommInterface {
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
+public class KVStore implements KVCommInterface
+{
+    private Logger logger = Logger.getRootLogger();
+    private boolean running;
+
+    private String serverAddress;
+    private int serverPort;
+    private Socket clientSocket;
+    private OutputStream receiveStream;
+    private InputStream sendStream;
 	
 	/**
 	 * Initialize KVStore with address and port of KVServer
 	 * @param address the address of the KVServer
 	 * @param port the port of the KVServer
 	 */
-	public KVStore(String address, int port) {
-		
+	public KVStore(String address, int port)
+	{
+	    this.serverAddress = address;
+	    this.serverPort = port;
 	}
+
+    public boolean isRunning() {
+        return this.running;
+    }
+
+    public void setRunning(boolean run) {
+        this.running = run;
+    }
 	
 	@Override
-	public void connect() throws Exception {
+	public void connect() throws Exception
+    {
 		// TODO Auto-generated method stub
-		
+        try
+        {
+            this.clientSocket = new Socket(this.serverAddress,this.serverPort);
+            setRunning(true);
+            receiveStream = this.clientSocket.getOutputStream();
+            sendStream = this.clientSocket.getInputStream();
+            logger.info("Connection established");
+        }
+        catch (Exception ex)
+        {
+            logger.info("Connection with " + this.serverAddress +
+                        ":" + this.serverPort + " not established");
+        }
 	}
 
 	@Override
-	public void disconnect() {
+	public void disconnect()
+    {
 		// TODO Auto-generated method stub
-		
+        logger.info("trying to close connection ...");
+
+        try
+        {
+            setRunning(false);
+            if (clientSocket != null)
+            {
+                //sendStream.close();
+                //receiveStream.close();
+                clientSocket.close();
+                clientSocket = null;
+                logger.info("connection closed!");
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.error("Unable to close connection!");
+        }
 	}
 
 	@Override
-	public KVMessage put(String key, String value) throws Exception {
+	public KVMessage put(String key, String value) throws Exception
+    {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public KVMessage get(String key) throws Exception {
+	public KVMessage get(String key) throws Exception
+    {
 		// TODO Auto-generated method stub
 		return null;
 	}
