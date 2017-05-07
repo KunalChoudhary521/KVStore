cb2a26368264692c04787f56f900ed9be4f5f846
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2017-05-07 00:07:01 -0400

    Object to be shared between KVServer & ClientConnection classes

---
+++ Milestone2/ScalableStorageService-stub/src/app_kvServer/ServerInfo.java
@@ -0,0 +1,93 @@
+package app_kvServer;
+
+import cache.KVCache;
+
+import java.nio.file.Path;
+
+//Holds variables that are shared between KVServer & ClientConnection objects
+public class ServerInfo
+{
+    private Path kvDirPath;
+    private Path mDataFile;
+    private KVCache cache;
+    private String startHash, endHash;
+    private Boolean running;
+
+    public ServerInfo()
+    {
+        startHash= null;
+        endHash = null;
+    }
+
+    public ServerInfo(Path dirPath, Path mDFile, KVCache cache,
+                      String sHash, String eHash, Boolean runState)
+    {
+        this.kvDirPath = dirPath;
+        this.mDataFile = mDFile;
+        this.cache = cache;
+        this.startHash = sHash;
+        this.endHash = eHash;
+
+        this.running = runState;
+    }
+
+    public void setKvDirPath(Path kvDirPath)
+    {
+        this.kvDirPath = kvDirPath;
+    }
+
+    public void setmDataFile(Path mDataFile)
+    {
+        this.mDataFile = mDataFile;
+    }
+
+    public void setCache(KVCache cache)
+    {
+        this.cache = cache;
+    }
+
+    public void setStartHash(String startHash)
+    {
+        this.startHash = startHash;
+    }
+
+    public void setEndHash(String endHash)
+    {
+        this.endHash = endHash;
+    }
+
+    public void setRunning(Boolean running)
+    {
+        this.running = running;
+    }
+
+    public Path getKvDirPath()
+    {
+        return kvDirPath;
+    }
+
+    public Path getmDataFile()
+    {
+        return mDataFile;
+    }
+
+    public KVCache getCache()
+    {
+        return cache;
+    }
+
+    public String getStartHash()
+    {
+        return startHash;
+    }
+
+    public String getEndHash()
+    {
+        return endHash;
+    }
+
+    public Boolean getRunning()
+    {
+        return running;
+    }
+}
